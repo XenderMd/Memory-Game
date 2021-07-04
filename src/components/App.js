196ea351e8c6cd96b6b3a32bf3558921dab4963f
XenderMd <denis.stavilamd@gmail.com>
2021-07-04 18:53:42 +0200

    First iteration - made the welcome page and the overall project structure

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import "../css/style.css";
+
+import React from 'react';
+
+import WelcomScreen from "./WelcomeScreen";
+
+const App = ()=>{
+
+    return(
+        <div className="app">
+            <WelcomScreen/>
+        </div>
+    )
+
+}
+
+export default App;
---
+++ src/components/WelcomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const WelcomScreen = ()=>{
+
+    return(
+        <div className="welcomeScreen">
+            <div className="welcomeScreen__text-box">
+                <h1 className="heading-primary heading-primary__main">The Memory Game</h1>
+                <h2 className="heading-primary heading-primary__sub">A game where you will be tested</h2>
+            </div>
+        </div>
+    )
+
+}
+
+export default WelcomScreen;
