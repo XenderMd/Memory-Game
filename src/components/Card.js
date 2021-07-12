20a543d445da8f66e1e30e7801824c0619cc3a87
XenderMd <denis.stavilamd@gmail.com>
2021-07-12 19:22:41 +0200

    Finished the Card and the CardDeck logic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,31 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
-const Card = ({image,id,onCardClicked})=>{
+const Card = ({image,id,onCardClicked, hideCard, locked, matched})=>{
 
     const [rotated, setRotated]=useState(false);
 
     const onSideClicked = ()=>{
-        onCardClicked(image,id);
-        setRotated(!rotated);
+
+        //If the cards were locked or already matched, do nothing
+        if(!locked && !matched){
+            onCardClicked(image,id);
+            setRotated(!rotated);
+        }
+        
     }
+
+    useEffect(()=>{
+
+        if(hideCard){
+            setRotated(false);
+        }
+
+    },[hideCard]);
 
     return(
         <div className="card">
-            <div onClick={onSideClicked} className={`card__side card__side--1 card__side--${rotated?"back":"front"}`}></div>
-            <div onClick={onSideClicked} className={`card__side card__side--2 card__side--${rotated?"front":"back"}`}>
+            <div onClick={onSideClicked} className={`card__side card__side--1 card__side--${!rotated?"front":"back"} ${matched?"card__side--matched":""}`}></div>
+            <div className={`card__side card__side--2 card__side--${rotated?"front":"back"}`}>
                 <img className="card__image" src={image} alt="card picture" ></img>
             </div>
         </div>
--- src/components/CardDeck.js
+++ src/components/CardDeck.js
@@ -8,34 +8,60 @@ import Card from './Card';
 
 const CardDeck = ({deckSize})=>{
 
-    const [deck, setDeck]=useState([]);
-    const [selection, setSelection]=useState({
-        id:'',
-        image:''
-    });
+    const [deckData, setDeckData]=useState([]);
     const [cardsHand, setCardsHand]=useState([]);
+    const [movesNum, setMovesNum]=useState(0);
+    const [hideCard, setHideCard]=useState(false);
+    const [locked, setLocked]=useState(false);
 
+    // Store selection
     const onCardClicked= (image,id)=>{
-        setSelection({id, image});
+        const selection = {id, image};
+        setCardsHand(prevCards => [...prevCards, selection].filter(selection=>selection.id!==""));
+        setMovesNum((prevMoves)=>{return prevMoves+1});
+        // setHideCard(false);
     }
 
-    //Deck initialization
+    // DeckData initialization
     useEffect(()=>{
         const cardNums = CardShufler(deckSize, Images);
-        setDeck(cardNums.map((cardNum)=>{return(<Card image={Images[cardNum]} id={uuidv4()} onCardClicked={onCardClicked}/>);}));
+        setDeckData(cardNums.map((cardNum)=>{return( {id:uuidv4(), image:Images[cardNum], matched:false})}));
     },[deckSize]);
 
-    //Record and store selection
+
+    // Process selection
+    // Lock the deck and do not allow the user to select other cards while the current pair is still visible
     useEffect(()=>{
-        setCardsHand(prevCards => [...prevCards, selection].filter(selection=>selection.id!==""));
-    },[selection]);
+        if(cardsHand.length===2){
+
+            setLocked(true);
+            setTimeout(()=>{setHideCard(true);}, 1000);
+
+            if(cardsHand[0].image===cardsHand[1].image){
+
+                const newDeckData = deckData.map((deckItem)=>{
+                    if (deckItem.id===cardsHand[0].id || deckItem.id===cardsHand[1].id){
+                        return {...deckItem, matched:true}
+                    } else{
+                        return deckItem;
+                    }
+                }); 
+                setDeckData(newDeckData);
+            }
+            setCardsHand([]);
+        }        
+    },[cardsHand, deckData]);
 
 
-    //Process selection
+    // Unlock the deck so that the user can make a new selection pair
     useEffect(()=>{
-        console.log(cardsHand);
-    },[cardsHand]);
+        setTimeout(()=>{
+            setHideCard(false);
+            setLocked(false);
+        }, 500);
+    },[hideCard]);
 
+    const deck=deckData.map((dataItem)=>{return(<Card image={dataItem.image} id={dataItem.id} matched={dataItem.matched} onCardClicked={onCardClicked} hideCard={hideCard} locked={locked}/>)});
 
     return(
         <div className="cardDeck">
